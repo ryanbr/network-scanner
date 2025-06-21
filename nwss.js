661570091503ff33835626d5f7a7cdc81c586307
ryanbr <rbrown@brave.com>
2025-06-21 11:13:56 +0000

    Bump version

--- nwss.js
+++ nwss.js
@@ -1,4 +1,4 @@
-// === Network scanner script (nwss.js) v1.0.23 ===
+// === Network scanner script (nwss.js) v1.0.24 ===
 
 // puppeteer for browser automation, fs for file system operations, psl for domain parsing.
 // const pLimit = require('p-limit'); // Will be dynamically imported
@@ -27,7 +27,7 @@ const { colorize, colors, messageColors, tags, formatLogMessage } = require('./l
 const { monitorBrowserHealth, isBrowserHealthy } = require('./lib/browserhealth');
 
 // --- Script Configuration & Constants ---
-const VERSION = '1.0.23'; // Script version
+const VERSION = '1.0.24'; // Script version
 const MAX_CONCURRENT_SITES = 3;
 const RESOURCE_CLEANUP_INTERVAL = 40; // Close browser and restart every N sites to free resources
 
