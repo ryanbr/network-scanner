3bdbffa8a543de0bc8dda9c2b96a9fd7bcce23f0
ryanbr <rbrown@brave.com>
2025-09-27 03:20:55 +0000

    Make window_cleanup realtime less aggressive and not close tabs too quickly, also add validation checks.

--- nwss.js
+++ nwss.js
@@ -1,4 +1,4 @@
-// === Network scanner script (nwss.js) v2.0.16 ===
+// === Network scanner script (nwss.js) v2.0.17 ===
 
 // puppeteer for browser automation, fs for file system operations, psl for domain parsing.
 // const pLimit = require('p-limit'); // Will be dynamically imported
@@ -130,7 +130,7 @@ const { navigateWithRedirectHandling, handleRedirectTimeout } = require('./lib/r
 const { monitorBrowserHealth, isBrowserHealthy, isQuicklyResponsive, performGroupWindowCleanup, performRealtimeWindowCleanup, trackPageForRealtime, updatePageUsage, cleanupPageBeforeReload } = require('./lib/browserhealth');
 
 // --- Script Configuration & Constants --- 
-const VERSION = '2.0.16'; // Script version
+const VERSION = '2.0.17'; // Script version
 
 // get startTime
 const startTime = Date.now();
