b1e03246735a8d1cb388fc816066cd78b5bae204
ryanbr <rbrown@brave.com>
2025-10-29 00:37:57 +0000

    Bump version

--- nwss.js
+++ nwss.js
@@ -1,4 +1,4 @@
-// === Network scanner script (nwss.js) v2.0.28 ===
+// === Network scanner script (nwss.js) v2.0.29 ===
 
 // puppeteer for browser automation, fs for file system operations, psl for domain parsing.
 // const pLimit = require('p-limit'); // Will be dynamically imported
@@ -143,7 +143,7 @@ const { navigateWithRedirectHandling, handleRedirectTimeout } = require('./lib/r
 const { monitorBrowserHealth, isBrowserHealthy, isQuicklyResponsive, performGroupWindowCleanup, performRealtimeWindowCleanup, trackPageForRealtime, updatePageUsage, cleanupPageBeforeReload } = require('./lib/browserhealth');
 
 // --- Script Configuration & Constants --- 
-const VERSION = '2.0.28'; // Script version
+const VERSION = '2.0.29'; // Script version
 
 // get startTime
 const startTime = Date.now();
